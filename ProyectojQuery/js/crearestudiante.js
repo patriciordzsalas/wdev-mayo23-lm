1bfbcd4125121e6261ea901449111845e5937f37
patriciordzsalas <113055267+patriciordzsalas@users.noreply.github.com>
2023-09-27 21:50:28 -0600

    Proyecto jQuery
    detalles finales

--- ProyectojQuery/js/crearestudiante.js
+++ ProyectojQuery/js/crearestudiante.js
@@ -4,59 +4,65 @@ var apicrearEst = "InsertarEstudiantes.php";
 const modalSuccessEst = new bootstrap.Modal(document.getElementById('modalSuccessEst'));
 
 $(document).ready(function () {
+  
 
-  $("#crearestudiante").click(function (e) { 
-    e.preventDefault();
-    //crearEst();
-
-  });
 });
 
 
-
-$("#formularioEst").submit(function (e) { 
+$("#crear").click(function (e) {     
+ 
     e.preventDefault();
+  
+      var datosEst ={    
+        "cedula" : $("#cedula").val(),
+        "correoelectronico" : $("#correoelectronico").val(),
+        "telefono" : $("#telefono").val(),
+        "telefonocelular" : $("#telefonocelular").val(),
+        "fechanacimiento" : $("#fechanacimiento").val(),
+        "sexo" : $("#sexo").val(),
+        "direccion" : $("#direccion").val(),
+        "nombre" : $("#nombre").val(),
+        "apellidopaterno" : $("#apellidopaterno").val(),
+        "apellidomaterno" : $("#apellidomaterno").val(),
+        "nacionalidad" : $("#nacionalidad").val(),
+        "idCarreras" : $("#idCarreras").val(),
+        "usuario" : "Patricio Rdz"
 
-    var datosEst ={    
-      cedula: $("#cedula").val() ,
-      correoelectronico: $("#correoelectronico").val() ,
-      telefonocelular: $("#telefonocelular").val() ,
-      fechanacimiento: $("#fechanacimiento").val() ,     
-      sexo: $("#sexo").val() ,
-      direccion: $("#direccion").val() ,
-      nombre: $("#nombre").val() ,
-      apellidopaterno: $("#apellidopaterno").val() ,     
-      apellidomaterno: $("#apellidomaterno").val() ,
-      nacionalidad: $("#nacionalidad").val() ,
-      idCarrera: $("#idCarreras").val() ,
-      usuario: "Patricio Rdz"
-    }
-    console.log(datosEst);
-    
-    
-    var apiurl = apibase + apicrearEst;
-    $.ajax({
-        type: "POST" ,
-        url: apiurl ,                
-        dataType: "json" ,
-        data: JSON.stringify(datosEst) ,
-        success: function (response) {
-            modalSuccessEst.show();
-            alert("Ingreso exitoso");
-            completeInsertEst();
-            console.log(response);
+      }
+      console.log(datosEst);
+      
+      enviardatos(datosEst);
+
+      
+
+      function enviardatos(datosEst) {
+       //alert("datos enviados");
+       var apiurl = apibase + apicrearEst;
+        $.ajax({
+            type: "POST" ,
+            url: apiurl ,                
+            dataType: "json" ,
+            data: JSON.stringify(datosEst) ,
+            success: function (response) {
+                modalSuccessEst.show();
+                alert("Ingreso exitoso");
+                completeInsertEst();
+                console.log(response);
+                
+            },
+            error: function ( xhr, textStatus, errorThrown){
+                console.log("Error ", errorThrown);
+            }
             
-        },
-        error: function ( xhr, textStatus, errorThrown){
-            console.log("Error ", errorThrown);
-        }
-        
+          
+        });
+
+      } 
+      
     });
 
-  });
-
-
 function completeInsertEst(){
-    window.location = 'listarestudiantes.html';
-
+  window.location = 'listarestudiantes.html';
+  
 }
+  
--- ProyectojQuery/js/crearprofesor.js
+++ ProyectojQuery/js/crearprofesor.js
@@ -1,59 +1,115 @@
-const modalSuccessProf = new bootstrap.Modal(document.getElementById('modalSuccessProf'))
-
 var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
 var apicrearProf = "InsertarProfesores.php";
 
-$(document).ready(function () {
-  
-    $("#crear").click(function (e) { 
-      e.preventDefault();
-      //crear();
+const modalSuccessProf = new bootstrap.Modal(document.getElementById('modalSuccessProf'));
 
-    });
+$(document).ready(function () {
+
 });
-$("#formularioProf").submit(function (e) { 
-    e.preventDefault();
+  
+$("#crear").click(function (e) { 
+
+  e.preventDefault();
 
     var datosProf ={
-      id: $("#id").val() ,     
-      cedula: $("#cedula").val() ,
-      correoelectronico: $("#correoelectronico").val() ,
-      telefonocelular: $("#telefonocelular").val() ,
-      fechanacimiento: $("#fechanacimiento").val() ,     
-      sexo: $("#sexo").val() ,
-      direccion: $("#direccion").val() ,
-      nombre: $("#nombre").val() ,
-      apellidopaterno: $("#apellidopaterno").val() ,     
-      apellidomaterno: $("#apellidomaterno").val() ,
-      nacionalidad: $("#nacionalidad").val() ,
-      idCarrera: $("#idCarreras").val() ,
-      usuario: "Patricio Rdz"
+      "id": $("#id").val() ,     
+      "cedula": $("#cedula").val() ,
+      "correoelectronico": $("#correoelectronico").val() ,
+      "telefono": $("#telefono").val() ,
+      "telefonocelular": $("#telefonocelular").val() ,
+      "fechanacimiento": $("#fechanacimiento").val() ,     
+      "sexo": $("#sexo").val() ,
+      "direccion": $("#direccion").val() ,
+      "nombre": $("#nombre").val() ,
+      "apellidopaterno": $("#apellidopaterno").val() ,     
+      "apellidomaterno": $("#apellidomaterno").val() ,
+      "nacionalidad": $("#nacionalidad").val() ,
+      "idCarreras": $("#idCarreras").val() ,
+      "usuario": "Patricio Rdz"
     }
     console.log(datosProf);
-    
-    var url = apibase + apicrearProf;
-    $.ajax({
-        type: "POST" ,
-        url: url ,                
-        dataType: "json" ,
-        data: JSON.stringify(datosProf) ,
-        success: function (datosProf) {
-            $(modalSuccessProf).show();
-            alert("Ingreso exitoso");
-            completeInsertProf()
-            console.log(datosProf);
-            
-        },
-        error: function ( xhr, textStatus, errorThrown){
-            console.log("Error ", errorThrown);
-        }
-        
-    });
 
-  });
+    crearprof(datosProf);
+
+    function crearprof(datosProf) {
+      alert("datos enviados");    
+      var url = apibase + apicrearProf;
+      $.ajax({
+          type: "POST" ,
+          url: url ,                
+          dataType: "json" ,
+          data: JSON.stringify(datosProf) ,
+          success: function (response) {
+              $(modalSuccessProf).show();
+              alert("Ingreso exitoso");
+              completeInsertProf();
+              console.log(response);
+              
+          },
+          error: function ( xhr, textStatus, errorThrown){
+              console.log("Error ", errorThrown);
+          }
+          
+      });
+    }
+});
 
 
 function completeInsertProf(){
     window.location = 'listarprofesores.html';
 
 }
+
+
+
+
+
+
+// var formulario = document.getElementById('formulario');
+
+// var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
+// var apicrear = "InsertarProfesores.php";
+// const myModalInsertar = new bootstrap.Modal(document.getElementById('myModalInsertar'));
+// var urlInsertar = apibase + apicrear;
+
+// $("#btnEnviar").click(function (e) { 
+//     e.preventDefault();
+    
+    
+
+
+//     var datosEnviar = {
+//         "cedula":$("#cedula").val(),
+//         "correoelectronico":$("#correoelectronico").val(),
+//         "telefono":$("#telefono").val(),
+//         "telefonocelular":$("#telefonocelular").val(),
+//         "fechanacimiento":$("#fechanacimiento").val(),
+//         "sexo":$("#sexo").val(),
+//         "direccion":$("#direccion").val(),
+//         "nombre":$("#nombre").val(),
+//         "apellidopaterno":$("#apellidopaterno").val(),
+//         "apellidomaterno":$("#apellidomaterno").val(),
+//         "nacionalidad":$("#nacionalidad").val(),
+//         "idCarreras":$("#idCarreras").val(),
+//         "usuario":$("#usuario").val()
+//     }
+
+//     $.ajax({
+//         type: "POST",
+//         url: urlInsertar,
+//         data: JSON.stringify(datosEnviar),
+//         dataType: "json",
+//         success: function (response) {
+//             completeInsert();
+//         },
+//         error: function (xhr, textStatus, errorThrown) {  
+//             console.log("Error: ", errorThrown );
+//         }
+//     });
+
+// });
+
+// function completeInsert(){
+//     myModalInsertar.show();
+//     window.location = 'listarprofesores.html';
+// }
