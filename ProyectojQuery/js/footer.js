5882b2ee6bfd40ba4b723e2caaf8431620b62f91
patriciordzsalas <113055267+patriciordzsalas@users.noreply.github.com>
2023-10-02 21:14:25 -0600

    CSS
    detalles en CSS

--- ProyectojQuery/js/footer.js
+++ ProyectojQuery/js/footer.js
@@ -1,7 +1,7 @@
 footer.innerHTML += `
-<div class="container">
+<div class="footer">
   <footer>
-    <div class="container">
+    <div class="container" id="footer">
       <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <p class="col-md-4 mb-0 text-muted">2023 WDEV-MAYO23-LM, VLA</p>
       </footer>
