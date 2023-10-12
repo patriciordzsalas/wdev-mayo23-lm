789c39ac1dedd33faed0e454c700a955fb9a48d6
patriciordzsalas <113055267+patriciordzsalas@users.noreply.github.com>
2023-10-11 20:51:38 -0600

    Angular
    Proyecto Angular

---
+++ Semana19/miercoles/dist/ejemplo.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Semana 19 Typescript");
+let nombre = "Clase miercoles";
+nombre = "90";
+let texto = "Patricio";
+let numero = 10;
+let booleano = true;
+function test(nombre) {
+    return "hola" + nombre;
+}
+let cars = ['toyota', 'nissan', 'bmw'];
---
+++ Semana19/miercoles/src/ejemplo.ts
@@ -0,0 +1,19 @@
+console.log("Semana 19 Typescript");
+
+let nombre : string = "Clase miercoles";
+
+nombre = "90";
+
+//Tipos de datos
+let texto: string = "Patricio";
+let numero: number = 10;
+let booleano: boolean = true;
+
+//Ejemplo
+function test(nombre: string ) {
+    return "hola" + nombre;
+}
+
+//test(numero);
+
+let cars: string[] = ['toyota','nissan', 'bmw'];
