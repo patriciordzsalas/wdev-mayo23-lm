59b2785fdf0bc4b1daa236fea4732f8b6b062da3
patriciordzsalas <113055267+patriciordzsalas@users.noreply.github.com>
2023-11-20 21:00:24 -0600

    React

---
+++ ProyectoReact/academia/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+
+
+import ListarCursos from './ComponentsCursos/index';
+
+function App() {
+  return (
+    <div>
+      <ListarCursos></ListarCursos>
+
+    </div>
+
+      
+  );
+}
+
+export default App;
